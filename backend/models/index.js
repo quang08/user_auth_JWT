deaf0e292f3c0b7b6b0201e6639ffc3e08d7035e
quang <nguyenthequang080203@gmail.com>
2023-10-01 21:16:36 +0700

    setup folder structure and sequelize

---
+++ backend/models/index.js
@@ -0,0 +1,20 @@
+const { config } = require('dotenv')
+const dbConfig = require('../config/db.config')
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(dbConfig.DB. dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        aquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+  },
+})
+
+const db = {}
+
+db.Sequelize = Sequelize
+db.sequelize = sequelize
