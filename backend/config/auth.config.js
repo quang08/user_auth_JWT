c74abbe30ec0c36f1d18f4b399f7e50a8ccfb9b1
quang <nguyenthequang080203@gmail.com>
2023-10-02 10:53:50 +0700

    add auth config

---
+++ backend/config/auth.config.js
@@ -0,0 +1,6 @@
+/*
+jsonwebtoken functions such as verify() or sign() use algorithm that needs a secret key (as String) to encode and decode token.
+*/
+module.exports = {
+  secret: "secret-key",
+};
