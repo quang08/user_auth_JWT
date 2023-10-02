a0a559016fdcd8bd67d2b8a24f625eda726fee82
quang <nguyenthequang080203@gmail.com>
2023-10-02 11:14:57 +0700

    add middleware for signup verification

--- backend/middleware/verifySignup.js
+++ backend/middleware/verifySignup.js
@@ -3,11 +3,47 @@
 - Check role existed or not
 */
 
-checkDuplicateUsernameOrEmail = (req, res, next) => {
+const db = require("../models/index");
+const User = db.user;
+const ROLES = db.ROLES;
+
+checkDuplicateUsernameOrEmail = async (req, res, next) => {
+  await User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({ message: "Failed. Username already existed." });
+      return;
+    }
+  });
+
+  await User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((user) => {
+    if (user) {
+      res.status(400).send({ message: "Failed. Email already existed." });
+      return;
+    }
+  });
   next();
 };
 
 checkRoleExisted = (req, res, next) => {
+  if (req.body.roles) {
+    for (let i = 0; i < req.body.roles.length; i++) {
+      if (!ROLES.includes(req.body.roles[i])) {
+        res.status(400).send({
+          message: "Failed, role does not exist: " + req.body.roles[i],
+        });
+        return;
+      }
+    }
+  }
+    
   next();
 };
 
